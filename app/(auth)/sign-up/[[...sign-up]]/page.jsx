f6227ca2120d74d25759e34e79fd4736121f1d00
praptisiva25 <161624277+praptisiva25@users.noreply.github.com>
2024-12-18 00:10:01 +0530

    17th Dec second one

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,13 +1,13 @@
 import { SignUp } from '@clerk/nextjs'
 
 // pages/auth/sign-up.js;
-import CustomSignUpForm from '../../../_components/CustomSignUpForm'; // Adjust path if needed
+//import CustomSignUpForm from '../../../_components/CustomSignUpForm'; // Adjust path if needed
 
 export default function SignUpPage() {
   return (
     <div>
-      <h1>Sign Up</h1>
-      <CustomSignUpForm />
+      <div><SignUp/></div>
+      
     </div>
   );
 }
